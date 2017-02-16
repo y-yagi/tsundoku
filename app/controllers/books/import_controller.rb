07c96efae129f4d74db8558ed3c0dd7c1c3df153
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-02-17 08:49:56 +0900

    implement import action

--- app/controllers/books/import_controller.rb
+++ app/controllers/books/import_controller.rb
@@ -1,4 +1,12 @@
 class Books::ImportController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
+    if params[:url].blank?
+      head :not_found
+    else
+      BookImporter.import(url: params[:url], user: current_user)
+      head :success
+    end
   end
 end
