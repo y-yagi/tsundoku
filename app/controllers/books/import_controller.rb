a942bdae670ea8743b7965ac3f4b5d4e51ed8dbe
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-02-17 10:04:59 +0900

    remove debug log

--- app/controllers/books/import_controller.rb
+++ app/controllers/books/import_controller.rb
@@ -2,7 +2,6 @@ class Books::ImportController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-Rails.logger.info("DEBUG: parmas: #{params.inspect}")
     if params[:url].blank?
       head :not_found
     else
