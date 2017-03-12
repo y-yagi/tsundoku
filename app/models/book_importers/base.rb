e046515d91ef98f40f159f2ae3746ff62f0dfa9f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-03-12 21:40:53 +0900

    remove duplicated codes

--- app/models/book_importers/base.rb
+++ app/models/book_importers/base.rb
@@ -1,8 +1,25 @@
 module BookImporters
   class Base
     class << self
+      def import(url:, user:)
+        agent = Mechanize.new
+        page = agent.get(url)
+
+        title = get_title(page)
+        raise "Unexpected Page: #{url}" unless title
+
+        Book.find_or_create_by!(title: title, user: user) do |book|
+          book.item_url = url
+          book.provider = provider
+        end
+      end
+
       def provider
         self.name.demodulize
+      end
+
+      def get_title(*)
+        raise NotImplementedError
       end
     end
   end
--- app/models/book_importers/gihyo_digital_publishing.rb
+++ app/models/book_importers/gihyo_digital_publishing.rb
@@ -1,17 +1,8 @@
 module BookImporters
   class GihyoDigitalPublishing < Base
     class << self
-      def import(url:, user:)
-        agent = Mechanize.new
-        page = agent.get(url)
-
-        title = page.search("h1#bookTitle").text
-        raise "Unexpected Page: #{url}" unless title
-
-        Book.find_or_create_by!(title: title, user: user) do |book|
-          book.item_url = url
-          book.provider = provider
-        end
+      def get_title(page)
+        page.search("h1#bookTitle").text
       end
     end
   end
