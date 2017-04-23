28aab68fff1a92ac1e62c0ac60c915b3461566f3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-24 07:49:29 +0900

    Use monkey patch version rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+// TODO: Temporary solution of https://github.com/rails/rails/pull/28837
+//= require monkey-rails-ujs
 //= require turbolinks
 //= require_tree .
