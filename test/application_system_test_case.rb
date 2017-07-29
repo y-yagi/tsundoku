7dfa8160d743ff053109b7cf8e7bfbd0bf24697e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-07-29 18:10:44 +0900

    use headless chrome in system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 require "capybara/poltergeist"
 
+Capybara.register_driver(:headless_chrome) do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless] }
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+  )
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :poltergeist
+  driven_by :headless_chrome, screen_size: [1400, 1400]
 
   def login
     visit '/auth/google_oauth2'
