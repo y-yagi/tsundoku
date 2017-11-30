c58bf28bf517ca13f9c881cd98f66e4e1f5de601
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-30 11:52:44 +0900

    use built-in headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium_chrome_headless
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   def login
     visit '/auth/google_oauth2'
