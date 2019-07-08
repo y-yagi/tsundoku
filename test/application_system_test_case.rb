8b128f589962f705309df67a0aa8eebf8976733e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-07-09 08:27:00 +0900

    Use `post` for login

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,6 +4,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   def login
-    visit '/auth/google_oauth2'
+    visit '/'
+    click_on 'Login with Google Account'
   end
 end
